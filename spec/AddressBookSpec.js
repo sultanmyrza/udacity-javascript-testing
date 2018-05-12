141cc56c47cbcc95d49c8f4684c41934f8277030
sultan <sultanmyrza@gmail.com>
2018-05-12 14:58:08 +0900

    refactor: place redundant test inits to beforeEach

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,16 +1,18 @@
 describe("AddressBook", function() {
-    it('Should add new contact to address book', function() {
-        var addressBook = new AddressBook(),
+    var addressBook,
+        newContact;
+        
+    beforeEach(function() {
+        addressBook = new AddressBook();
         newContact = new Contact();
+    });
 
+    it('Should add new contact to address book', function() {
         addressBook.addContact(newContact);
         expect(addressBook.getContact(0)).toBe(newContact);
-    })
+    });
 
     it('Should be able to delete contact', function() {
-        var addressBook = new AddressBook(),
-        newContact = new Contact();
-
         addressBook.addContact(newContact);
         var contactsBefore = addressBook.getContacts().length;
         addressBook.removeContact(newContact);
